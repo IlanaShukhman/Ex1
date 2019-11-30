3da6de80012abf46e14949fea9766e0d51f75231
Ilana Shukhman <Ilana77001@gmail.com>
2019-11-30 14:06:26 +0200

    Add JUnit tests outline

---
+++ src/Ex1Tests/ComplexFunctionTest.java
@@ -0,0 +1,84 @@
+package Ex1Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ComplexFunctionTest {
+
+	@Test
+	final void testF() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testComplexFunction() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testComplexFunctionOperationFunctionFunction() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testInitFromString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testOperationAnalyzing() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testCopy() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testPlus() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testMul() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testDiv() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testMax() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testMin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testComp() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testLeft() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testRight() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetOp() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
