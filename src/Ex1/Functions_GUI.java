060c2e095822c4eeaade179938dff433705aae61
Ilana Shukhman <Ilana77001@gmail.com>
2019-12-06 14:47:18 +0200

    start drawing

--- src/Ex1/Functions_GUI.java
+++ src/Ex1/Functions_GUI.java
@@ -1,5 +1,7 @@
 package Ex1;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -186,8 +188,59 @@ public class Functions_GUI implements functions {
 
 	@Override
 	public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
-		// TODO Auto-generated method stub
+		// number of line segments to plot
+		int n=resolution;
+		int range=(int) (rx.get_max()-rx.get_min());
+		double maxY = ry.get_max(), minY = ry.get_min();
+		int j=0;
+		Iterator itr= this.iterator();
+		while(itr.hasNext()) {
+			
+			function f= this.func_collection.get(j);
+			
+			double[] x = new double[(int) (n+1)];
+			double[] y = new double[(int) (n+1)];
 
+			for (int i = 0; i <= n; i++) {
+				x[i] = range/n;
+				y[i] = f.f(x[i]);
+			}		
+			// rescale the coordinate system
+			StdDraw.setXscale(0, Math.PI);
+			StdDraw.setYscale(minY, maxY);
+
+			//////// vertical lines
+			StdDraw.setPenColor(Color.LIGHT_GRAY);
+			for (int i = 0; i <= n; i=i+10) {
+				StdDraw.line(x[i], minY, x[i], maxY);
+			}
+			//////// horizontal  lines
+			for (double i = minY; i <= maxY; i=i+0.5) {
+				StdDraw.line(0, i, Math.PI, i);
+			}
+			//////// x axis		
+			StdDraw.setPenColor(Color.BLACK);
+			StdDraw.setPenRadius(0.005);
+			StdDraw.line(0, y[(int) (n/2)], Math.PI, y[(n/2)]);
+			StdDraw.setFont(new Font("TimesRoman", Font.BOLD, 15));
+			for (int i = 0; i <= n; i=i+10) {
+				StdDraw.text(x[i]-0.07, -0.07, Integer.toString((i-n/2)));
+			}
+			//////// y axis	
+			StdDraw.line(x[(n/2)], minY, x[(n/2)], maxY);
+			for (double i = minY; i <= maxY; i=i+0.5) {
+				StdDraw.text(x[(n/2)]-0.07, i+0.07, Double.toString(i));
+			}
+
+			// plot the approximation to the function
+			for (int i = 0; i < n; i++) {
+				StdDraw.line(x[i], y[i], x[i+1], y[i+1]);
+			}
+			StdDraw.setPenColor(Color.RED);
+			StdDraw.setPenRadius(0.01);
+			StdDraw.point(x[(n/2)], 1);
+
+		}
 	}
 
 	@Override
